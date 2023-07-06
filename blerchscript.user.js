4b948c7b39adc5c4aeae45055a756a14bc7727ad
blerch-dev <blerch.dev@gmail.com>
2023-07-05 20:26:20 -0500

    Fixed Embed Block

--- blerchscript.user.js
+++ blerchscript.user.js
@@ -29,10 +29,12 @@ function bs_run() {
     const CreateIframe = (url) => {
         let f = document.createElement('iframe');
         f.src = GetURL(url);
-        f.style = "border: medium none; overflow: hidden; width: 100%; height: auto;";
+        f.style = "border: medium none; overflow: hidden; width: 100%; height: 100%;";
         f.scrolling = "no";
         f.allowFullscreen = "true";
         f.allow = "autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share";
+
+        return f;
     };
 
     const SetCustomEmbed = (url) => {
@@ -50,22 +52,26 @@ function bs_run() {
         }
         console.log(iframe);
 
+      	observer.disconnect();
         if(iframe instanceof Element) {
             iframe.src = GetURL(url);
         } else {
             iframe = CreateIframe(url);
             wrap.appendChild(iframe);
         }
+
+      	observer.observe(iframe, { attributes: true });
+      	//document.getElementById('abyss-shield').style.display = 'none';
     };
 
     const CreateInput = (parent) => {
-        if(!(parent instanceof Element)) { 
+        if(!(parent instanceof Element)) {
             return DebugLog("No instance of parent element.", parent);
         }
-        
+
         let ci = document.getElementById('bs-direct-player-elem')
         if(ci instanceof Element) {
-            return DebugLog("Already instance of input, should be set up.", ci); 
+            return DebugLog("Already instance of input, should be set up.", ci);
         }
 
         let elem = document.createElement('div');
@@ -94,10 +100,10 @@ function bs_run() {
 
         let desc = document.createElement('p');
         desc.style.padding = "15px";
-        desc.innerHTML = 'Controls:<br><br>- Backquote to hide/show<br>- Enter/NumpadEnter to set iframe source' +
+        desc.innerHTML = 'Controls:<br><br>- Backquote to hide/show<br>- Enter/NumpadEnter to set iframe source<br>- P to Randomize Highlights' +
         '<br><br>Requires direct player url (iframe source)<br>Examples:<br><br>#twitch/destiny = https://player.twitch.tv/?channel=destiny' +
         '<br>#youtube/G9rLVTzlGdY = https://www.youtube.com/embed/G9rLVTzlGdY?autoplay=1&playsinline=1' +
-        '<br>#strims.gg/zlxb = whatever the source at https://strims.gg/zlxb is (usually https://player.angelthump.com/?channel=zlxb)';
+        '<br>strims.gg/zlxb = whatever the source at https://strims.gg/zlxb is (usually https://player.angelthump.com/?channel=zlxb)';
         //<div title="GIGACHAD" class="emote GIGACHAD">GIGACHAD </div><div title="UNLUCKY" class="emote UNLUCKY">UNLUCKY </div>
         elem.appendChild(desc);
         parent.appendChild(elem);
@@ -111,16 +117,68 @@ function bs_run() {
     };
 
     document.addEventListener('DOMContentLoaded', () => {
+      	document.getElementById("stream-block").remove();
+      	document.getElementById("offline-text").remove();
+      	document.getElementById("embed").remove();
+      
+      	// url
+      	//DebugLog(window.location);
+      	let embed = window.location.hash?.split('/');
+      	if(embed[0] === "#youtube") {
+          SetCustomEmbed(`https://www.youtube.com/embed/${embed[1]}?autoplay=1&playsinline=1&hd=1`);
+        } else if(embed[0] === '#twitch') {
+          SetCustomEmbed(`https://player.twitch.tv/?channel=${embed[1]}`);
+        } else if(embed[0] === "#custom") {
+          SetCustomEmbed(embed[1]);
+        }
+      
         document.addEventListener('keydown', (e) => {
-            DebugLog(e.target.id, e.key, e.target?.id === "bs-direct-player-input", e.key === "Enter");
+            //DebugLog(e.target.id, e.key, e.target?.id === "bs-direct-player-input", e.key === "Enter");
             if(e.code === 'Backquote') {
                 ToggleInput();
             } else if(e.target?.id === "bs-direct-player-input" && e.key === "Enter") {
                 SetCustomEmbed(e.target.value);
+            } else if(e.code === 'KeyP') {
+              enable_highlight_randomizer = !enable_highlight_randomizer;
+              if(enable_highlight_randomizer)
+                DebugLog("Enabled Highlight Randomizer");
+              else
+                DebugLog("Disabled Highlight Randomizer");
+            } else {
+              //DebugLog("Code:", e.code);
             }
         });
-    
+
         DebugLog('Starting Script...........');
         CreateInput(document.getElementById('stream-panel'));
-    });
+
+      let chat_list = document.getElementsByClassName('chat-lines')[0];
+      if(chat_list instanceof Element) {
+        	DebugLog("Connected Chat List");
+      		chat_obv.observe(chat_list, { childList: true });
+      	} else {
+          DebugLog("Could not find chat list.");
+        }
+      });
+
+  const observer = new MutationObserver((mut) => {
+    DebugLog('IFrame Mutation:', mut);
+  });
+
+  const highlight_prob = 60;
+  var enable_highlight_randomizer = false;
+  const chat_obv = new MutationObserver((mut) => {
+    if(!enable_highlight_randomizer)
+      return;
+
+    let elem = mut[0]?.addedNodes[0];
+    if(elem instanceof Element) {
+      let value = Math.random() * 100;
+      if(value < highlight_prob) {
+        let ret = elem.classList.add('msg-highlight');
+      }
+    } else {
+      DebugLog("Not Element:", elem);
+    }
+  });
 }
